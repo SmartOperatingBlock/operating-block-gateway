6bb51391332f739a4a7ef07920775b222530fff3
giacomoaccursi <52125090+giacomoaccursi@users.noreply.github.com>
2023-02-14 19:37:21 +0100

    chore: add command to clean configFiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,5 +4,6 @@ docker rm operating-block-gateway-rebuilded
 docker run -d --rm --name operating-block-gateway --device /dev/ttyUSB0:/dev/ttyUSB0 -v ./data:/data -p 1880:1880 -e NODE_RED_ENABLE_SAFE_MODE=true nodered/node-red
 docker exec operating-block-gateway /bin/sh -c "cd /data; npm install node-red-contrib-https; npm install node-red-node-serialport"
 docker exec operating-block-gateway /bin/sh -c "cd /data; npm rebuild --build-from-source"
+docker exec operating-block-gateway /bin/sh -c "cd /data/configFiles; echo -n '' > *"
 docker stop operating-block-gateway
 docker run -p 1880:1880 -v ./data:/data --device /dev/ttyUSB0:/dev/ttyUSB0 --group-add dialout --env-file .env --name operating-block-gateway-rebuilded nodered/node-red
